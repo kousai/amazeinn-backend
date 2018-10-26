123a8fdebcdacd26859f21491ec95c024663c78e
kousai <yukarichin@hotmail.com>
2018-10-26 23:15:42 +0800

    modify

--- main.rb
+++ main.rb
@@ -1,21 +1,67 @@
 require 'sinatra'
+require 'slim'
 require_relative 'models/init'
 require_relative 'routes/init'
 
+use Rack::Session::Pool, :expire_after => 3600
+
 get '/' do
-  _index = ""
-  _brief_summary = "<h1>Brief Summary</h1>"
-  _brief_guests = "<p>Here are #{Guest.count.to_s} guests, and #{Guest.count(:isreal => true).to_s} real guests!</p>"
-  _brief_messages = "<p>Here are #{Message.count.to_s} messages!</p>"
-  _brief_contacts = "<p>Here are #{Contact.count.to_s} contacts!</p>"
-  _brief_thumbs = "<p>Here are #{Thumb.count.to_s} thumbs!</p>"
-  _brief_rooms = "<p>Here are #{Room.count.to_s} Rooms, and #{Room.count(:isempty => true).to_s} empty rooms!</p>"
-  _brief_summary << _brief_guests << _brief_messages << _brief_contacts << _brief_thumbs << _brief_rooms
-  _list_guests = "<h2>List of Guests</h2><table border='1'><tr><th>id</th><th>name</th><th>gender</th></tr>"
-  _guests = Guest.all
-  _guests.each do |guest|
-    _list_guests << "<tr><td>#{guest._id.to_s}</td><td>#{guest.name}</td><td>#{guest.gender}</td></tr>"
+  @brief_guests = "Here are #{Guest.count.to_s} guests, and #{Guest.count(:isreal => true).to_s} real guests!"
+  @brief_messages = "Here are #{Message.count.to_s} messages!"
+  @brief_contacts = "Here are #{Contact.count.to_s} contacts!"
+  @brief_thumbs = "Here are #{Thumb.count.to_s} thumbs!"
+  @brief_rooms = "Here are #{Room.count.to_s} Rooms, and #{Room.count(:isempty => true).to_s} empty rooms!"
+  @isLogedIn = !session[:value].nil?
+  slim :index
+end
+
+post '/admin/login' do
+  _body = JSON.parse(request.body.read)
+  if _body["account"] == "admin" && _body["password"] == "amazeinn"
+    session[:value] = "admin"
+    _guests = Guest.all
+    _messages = Message.all
+    _contacts = Contact.all
+    _thumbs = Thumb.all
+    _rooms = Room.all
+    {guests: _guests, messages: _messages, contacts: _contacts, thumbs: _thumbs, rooms: _rooms}.to_json
+  else
+    400
   end
-  _list_guests << "</table>"
-  _index << _brief_summary <<_list_guests
+end
+
+post '/admin/logout' do
+  session.clear
+end
+
+post '/admin/new' do
+  200
+end
+
+post '/admin/update' do
+  200
+end
+
+post '/admin/delete' do
+  200
+end
+
+post '/admin/drop' do
+  _body = JSON.parse(request.body.read)
+  case _body["schema"]
+  when "Guest" then Guest.auto_migrate!
+  when "Message" then Message.auto_migrate!
+  when "Contact" then Contact.auto_migrate!
+  when "Thumb" then Thumb.auto_migrate!
+  when "Room" then Room.auto_migrate!
+  end
+end
+
+post '/admin/init' do
+  _body = JSON.parse(request.body.read)
+  case _body["type"]
+  when "random" then init_random(_body["amount"])
+  when "room" then init_room(_body["amount"])
+  when "mock" then init_mock(_body["amount"])
+  end
 end
